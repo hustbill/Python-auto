e06c298a25052adba955c6ff6a7529f095659f99
hustbill <billyzhang2010@gmail.com>
2017-02-25 15:16:06 -0800

    use random module

---
+++ learn/data-science-from-scratch/randomness.py
@@ -0,0 +1,6 @@
+import random
+
+four_uniform_randoms = [random.random() for _ in range(4)]
+# random.random() produces numbers uniformly between 0 and 1
+# it's the random function we'll use most ofen
+print four_uniform_randoms
